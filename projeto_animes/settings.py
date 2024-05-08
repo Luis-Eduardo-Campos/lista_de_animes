b493865847226ed4d9defe78f9126ac3b915ee0e
Luis Eduardo Campos <lecamposnk@gmail.com>
2024-05-07 23:34:34 -0300

    preparando para o render

--- projeto_animes/settings.py
+++ projeto_animes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%1-g4h0pmzv!qv16ef!4lrv!jaoawpfeg7!s@zyj#jb4b$^t0='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
